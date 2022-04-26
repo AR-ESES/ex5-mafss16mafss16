5575585503427ef30e9103708f5ce61dee152935
mafss16mafss16 <102663606+mafss16mafss16@users.noreply.github.com>
2022-04-26 18:01:16 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let laranjas;
+	
+	function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0)
+	
+	}
+	
+	function draw() {
+	
+	background(0)
+	if(mouseY>=height/2 && mouseX<=width/2) {
+	
+	fill (131,111,255);
+	} 
+	else if (mouseY<height/2 && mouseX<=width/2) {
+	
+	fill (255,0,0); 
+	}
+	else if(mouseY<height/2 && mouseX>width/2) { 
+	
+	fill (0,255,0); 
+	} 
+	else{
+	noFill();
+	}
+	
+	stroke(230);
+	strokeWeight(2);
+	circle(width / 2, height / 2, height / 6);
+	
+	
+	}
+
